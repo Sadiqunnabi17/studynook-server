0cae565b101d7e8ca0730c0d60c31bab883bbe22
Sadiqunnabi17 <sadiqunnabi@gmail.com>
2026-05-22 09:18:50 +0600

    Backend: simplify cookie options for production cross-origin

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -6,8 +6,10 @@ const User = require("./user.model");
 
 const cookieOptions = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  // secure: process.env.NODE_ENV === "production",
+  // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  secure: true,
+  sameSite: "none",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
 
